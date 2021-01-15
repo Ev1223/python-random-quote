f0fcadcf946ca6316e36c689ab99ea066b7af699
KZN IT Support <kznitsupport@dontwaste.co.za>
2021-01-15 12:21:40 +0200

    random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,12 @@
+import random
 def misty():
-   print("hello world.")
-
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
-
-  #print(quotes)
+  
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
+  last = 13
+  rnd = random.randint(0, last) 
+  print(quotes[rnd])
 
 if __name__== "__main__":
   misty()
